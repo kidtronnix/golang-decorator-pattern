76e8497672e2f955a8005ea5974e8628e2433d67
smaxwellstewart <smaxwellstewart@gmail.com>
2016-03-02 23:15:19 -0500

    removed constructor to make a little more transparent/flexible

--- client/client.go
+++ client/client.go
@@ -1,10 +1,6 @@
 package client
 
-import (
-	"log"
-	"net/http"
-	"os"
-)
+import "net/http"
 
 // Our client is defined as something that does a http request and gets a response and error
 type Client interface {
@@ -17,12 +13,4 @@ type ClientFunc func(*http.Request) (*http.Response, error)
 // Add method to DoFunc type to satisfy Client interface
 func (f ClientFunc) Do(r *http.Request) (*http.Response, error) {
 	return f(r)
-}
-
-// NewClient will return a new client complete with all our decorators.
-func NewClient() Client {
-	return Decorate(MockClient{},
-		Logging(log.New(os.Stdout, "[logging decorator] ", 0)),
-		Audit(log.New(os.Stdout, "[audit decorator] ", 0)),
-	)
 }
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/smaxwellstewart/golang-decorator-pattern/client"
 )
 
-// Just a simple http client, right?
+// We can construct a client with all the functionality we need.
 func main() {
-	c := client.NewClient()
+	c := client.Decorate(client.MockClient{},
+		client.Logging(log.New(os.Stdout, "[logging decorator] ", 0)),
+		client.Audit(log.New(os.Stdout, "[audit decorator] ", 0)),
+	)
 	req, _ := http.NewRequest("GET", "http://example.com", nil)
 	c.Do(req)
 }
